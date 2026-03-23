015938797a51a54133c859906c25686454fafce7
Enrique Torrella <ktorrellagil@gmail.com>
2026-03-23 19:17:45 +0100

    fix: streamline redirect logic in web routes

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return auth()->check()
-        ? redirect()->route('home')
-        : redirect()->route('login');
-});
-
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return redirect()->route(auth()->check() ? 'home' : 'login');
+});
+
+Route::get('/home', [HomeController::class, 'index'])
+    ->middleware('auth')
+    ->name('home');
